a37ef83c9b358941d8a478521ce264263df222e6
Wes Lauer <lauerj@seattleu.edu>
2022-01-31 02:04:41 -0800

    updated find reference voltage

--- Conductivity/findVref.py
+++ Conductivity/findVref.py
@@ -1,26 +1,32 @@
+#import libraries
 import machine
 from machine import Pin, ADC
 import time
-import math
-import array as arr
 
-adc = ADC(Pin(10))
-adc.atten(ADC.ATTN_11DB)
+#specify parameters
+n = 20 #number of samples to average
+R1 = 10000
+R2 = 10000
+v33 = 3.30
+#compute v_data
+v_data = 3.3*R2/(R1+R2)
 
-R1 = 8970
-R2 = 9030
-V33 = 3.3
-V = 3.3*R2/(R1+R2)
-readings = []
-maxcount = 8191
+#initialize ADC
+try:
+    #works for ESP8266
+    adc = ADC(0)
+    maxcount = 1023
+except:
+    #works for ESP32
+    adc = ADC(Pin(5))
+    adc.atten(ADC.ATTN_11DB)
+    maxcount = 8191  #4095 if ESP32
 
-n = 0
+#Collect n measurements, average, and print results
 total = 0
-while True:
-    n = n+1
+for i in range(20):
     total = total + adc.read()
-    average = total / n 
-       
-    vmax = V * maxcount / average
-    
-    print(f"average = {average}, vmax = {vmax}")
+    time.sleep(0.2) #necessary to give ADC time to reset
+c_adc_av = total / n 
+v_ref = v_data * maxcount / c_adc_av
+print(f"n = {n}, average ADC count = {c_adc_av}, average Vref = {v_ref}")

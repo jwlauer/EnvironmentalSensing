c092abdc8945e29e99ee961319627f74b759ae21
Wes Lauer <lauerj@seattleu.edu>
2022-02-24 01:05:10 -0800

    Rain logger

--- RainLogger/main.py
+++ RainLogger/main.py
@@ -1,22 +1,22 @@
-"""Logs count and time to a Google Sheet using IFTTT.
+"""Logs count and time to a Google Sheet
 Wes Lauer
-February 21, 2022
+February 23, 2022
 Released under MIT license
 
-This program uses an ESP32 to to post a timestamp and count to 
-a Google Sheet. It is intended to run on
-a Feather ESP32-S2 board (which has a built-in connection between
-battery and the on-board LC709203F battery  monitor). It was written as a way
+This program uses an ESP32 to to post a timestamp and count to a
+Google Sheet. It is intended to run on a Feather ESP32-S2 board
+(which has a built-in connection between battery and the on-board
+LC709203F battery  monitor). It was written as a way
 of logging the time when the bucket in a tipping bucket rain gage
 empties itself. To conserve power, the program
 spends most of its time in a deep sleep state and only wakes
 when an event occurs, resetting the board. The rst pin should be
 connected to ground when a tip occurs.
-To make it easy to break out of the program, it waits a little longer
-at the end of the program when a tip/reset occurs.  
+To make it easy to break out of the program, it waits a little
+longer at the end of the program when a tip/reset occurs.  
 
-Be sure to set the correct SSID, password and keys to the Google App Script
-and Google Sheet.
+Be sure to set the correct SSID, password and keys to the Google
+App Script and Google Sheet.
 
 """
 
@@ -48,11 +48,11 @@ power.value(1)
 
 #read the value of the count
 try:
-    f = open('count.txt')
+    f = open("count.txt")
     total = int(f.read())
 #the except clause creates the file if it doesn't exist
 except:
-    f = open('count.txt', 'w')
+    f = open("count.txt", "w")
     total = 0
 f.close()
 
@@ -60,21 +60,21 @@ f.close()
 wake_reason = machine.wake_reason() #not used, but here for illustration
 reset_cause = machine.reset_cause()
 if reset_cause == 1: #machine.PWRON_RESET:
-    print('Woke due to reset, incrementing total')
+    print("Woke due to reset, incrementing total")
     total = total + 1
     #then write new count back to the count file
-    print('New Total = %s\r\n' % total)
-    f = open('count.txt', 'w') #this erases the old file
+    print("New Total = %s\r\n" % total)
+    f = open("count.txt", "w") #this erases the old file
     f.write(str(total)) #only first line of file is used
     f.close()
     print(f"Saved new total after {(utime.ticks_ms()-startticks)/1000:.2f} seconds")
     flash_led(10,50)
 elif reset_cause == 2:
-    print('Woke from hard reset or machine.reset, so not incrementing total')
+    print("Woke from hard reset or machine.reset, so not incrementing total")
 elif reset_cause == 5:
-    print('Woke from soft reset (as in Thonny Stop), so not incrementing total')
+    print("Woke from soft reset (as in Thonny Stop), so not incrementing total")
 elif reset_cause == 4: #machine.DEEPSLEEP_RESET
-    print('Woke from deepsleep, so not incrementing total')
+    print("Woke from deepsleep, so not incrementing total")
     
 #read value of voltage
 from machine import I2C, Pin
@@ -94,15 +94,12 @@ except:
     byte_tmsb = i2c.readfrom_mem(0x68,0x11,1)
     byte_tlsb = i2c.readfrom_mem(0x68,0x12,1)
     tempC = list(byte_tmsb)[0]+list(byte_tlsb)[0]/256
-print(f'temp = {tempC}')
+print(f"temp = {tempC}")
     
-
-
 #connect to wlan
 sta_if = network.WLAN(network.STA_IF)
 if not sta_if.isconnected():
     try:
-        #sta_if = network.WLAN(network.STA_IF) #define object to access station mode functions
         sta_if.active(True) #make station mode active
         sta_if.connect(ssid, password)
         utime.sleep(1)
@@ -123,11 +120,11 @@ try:
     i2c = I2C(1,scl=Pin(4), sda=Pin(3), freq=100000)
     rtc = urtc.DS3231(i2c)
     datetime = rtc.datetime()
-    print('Time set from DS3231')
+    print("Time set from DS3231")
 except:
     try:
         from ntptime import settime
-        print('Time set from NTP time to: ', end = '')
+        print("Time set from NTP time to: ", end = "")
         settime()
         utime.sleep_ms(300)
         rtc = machine.RTC()
@@ -138,10 +135,10 @@ except:
     datetime = rtc.datetime()
 
 #save string to log file
-write_string = '%s/%s/%s,' % (datetime[0], datetime[1], datetime[2])
-write_string += '%s:%s:%s,' % (datetime[4], datetime[5], datetime[6])
-write_string += '%s,%s,%s,%s\r\n' % (total, battery, reset_cause, tempC)
-f = open('rainlog.txt', 'a')
+write_string = "%s/%s/%s," % (datetime[0], datetime[1], datetime[2])
+write_string += "%s:%s:%s," % (datetime[4], datetime[5], datetime[6])
+write_string += "%s,%s,%s,%s\r\n" % (total, battery, reset_cause, tempC)
+f = open("rainlog.txt", "a")
 f.write(write_string)
 f.close()
 
@@ -149,22 +146,19 @@ flash_led(5,150)
 
 #format data for posting online
 data = {}
-data['Field1'] = total
-data['Field2'] = battery
-data['Field3'] = reset_cause
-data['Field4'] = tempC
-data['Sheet_id'] = gSheetKey
-url = 'https://script.google.com/macros/s/'+gKey+'/exec'
+data["Field1"] = total
+data["Field2"] = battery
+data["Field3"] = reset_cause
+data["Field4"] = tempC
+data["Sheet_id"] = gSheetKey
+url = "https://script.google.com/macros/s/"+gKey+"/exec"
 
 for n in range(4):
     try:
-        #print('trying request n = ',n)
-        #request = prequests.post(url, json = data)
         flash_led(3,75)
         request = prequests.post(url, json = data)
         utime.sleep(1)
         flash_led(5,75)
-        #outcome = ('Posted with reason: ',request.reason)
         outcome = 'Posted OK'
         request.close()
         break
@@ -176,8 +170,8 @@ for n in range(4):
             try:
                 request.close()
             except:
-                print('request did not exist when closing')
-        outcome = 'Post failed'
+                print("request did not exist when closing")
+        outcome = "Post failed"
 print(outcome)
 
 #turn off wifi to lower power when in sleep mode
@@ -191,11 +185,6 @@ else:
     flash_led(4,50)
     
 timeon = (utime.ticks_ms() - startticks)/1000
-print('Going to sleep after being awake for ' + str(timeon) + ' s')
+print("Going to sleep after being awake for " + str(timeon) + " s")
 utime.sleep(0.25)
 machine.deepsleep(1000*5*60) 
-
-
-
-
-
